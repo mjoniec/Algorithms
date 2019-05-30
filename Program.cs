d9d6cb4ee6bbd03da65e413c3c60baeef451d706
mjoniec <mart1n.joniec@gmail.com>
2019-05-30 23:35:37 +0200

    add pdf docx to pdf class

--- Program.cs
+++ Program.cs
@@ -308,4 +308,48 @@ test end
             return isPresent;
         }
     }
+    
+     /// <summary>
+    /// when constructor should throw exception ttps://stackoverflow.com/questions/77639/when-is-it-right-for-a-constructor-to-throw-an-exception
+    /// </summary>
+    public class DocxToPdfConverter
+    {
+        private const string DocxExtension = ".docx";
+        private readonly string _pathToFolderWithDocuments;
+
+        /// <summary>
+        /// When path non existing throws exception
+        /// </summary>
+        /// <param name="pathToFolderWithDocuments"></param>
+        public DocxToPdfConverter(string pathToFolderWithDocuments)
+        {
+            if (!Directory.Exists(pathToFolderWithDocuments))
+            {
+                throw new DirectoryNotFoundException(pathToFolderWithDocuments);
+            }
+
+            _pathToFolderWithDocuments = pathToFolderWithDocuments;
+        }
+
+        public string ConvertAllFiles()
+        {
+            var fileEntries = Directory.GetFiles(_pathToFolderWithDocuments);
+
+            if (fileEntries.All(fileName =>
+                !string.Equals(DocxExtension, Path.GetExtension(fileName))))
+            {
+                //exceptions are costly
+                //throw new FileNotFoundException("No .docx documents available at: " + _pathToFolderWithDocuments);
+
+                return "No .docx documents available at: " + _pathToFolderWithDocuments;
+            }
+
+            //foreach (var fileName in fileEntries)
+            //{
+            //    ProcessFile(fileName);
+            //}
+
+            return "xxx";
+        }
+    }
 }
