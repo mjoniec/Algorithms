f038c638b18437ec40a1cd3c78c7169f65596cf2
mjoniec <mart1n.joniec@gmail.com>
2019-06-07 23:43:11 +0200

    add launcher net framework

---
+++ Launcher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var launcher = new Launcher();
+            var samples = launcher.GetAllSamplesNames();
+            var sampleName = samples.First();
+
+            var result = launcher.RunSample(sampleName);
+
+            Console.WriteLine(result);
+        }
+    }
+}
