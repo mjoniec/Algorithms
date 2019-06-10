58a98779b4e5e8412e17f64cdce095c51399c2ce
mjoniec <mart1n.joniec@gmail.com>
2019-06-10 18:01:48 +0200

    Update Program.cs

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Linq;
-
 namespace Launcher
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var launcher = new Launcher();
-            var samples = launcher.GetAllSamplesNames();
-            var sampleName = samples.First();
-
-            var result = launcher.RunSample(sampleName);
-
-            Console.WriteLine(result);
+            new Launcher().Run();
         }
     }
 }
