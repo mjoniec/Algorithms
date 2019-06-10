fba872101a3f605813c6a8197b007e0fe3610edb
mjoniec <mart1n.joniec@gmail.com>
2019-06-10 18:01:23 +0200

    Update Launcher.cs

--- Launcher/Launcher.cs
+++ Launcher/Launcher.cs
@@ -1,5 +1,6 @@
-﻿using Reusables;
+using Reusables;
 using Samples;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,18 +14,87 @@ namespace Launcher
         {
             _codeSamples = new List<ILaunchableSample>
             {
-                new DocxToPdfConverter(),
                 new ThreadingTest(),
                 new MergeCommonElementsDistinct()
             };
         }
 
-        public List<string> GetAllSamplesNames()
+        public void Run()
+        {
+            MainLoop();
+        }
+
+        private void MainLoop()
+        {
+            while (true)
+            {
+                Console.Clear();
+                PrintAvaliableCodeSamples();
+
+                var input = Console.ReadLine();
+
+                Console.Clear();
+
+                var sampleNameToRun = TryParseInputIntoSampleNameToRun(input);
+
+                if (IsEndingCondition(sampleNameToRun)) break;
+
+                Console.WriteLine("Running sample: " + sampleNameToRun);
+                Console.WriteLine("Press any key to clear and return.");
+
+                RunSample(sampleNameToRun);
+
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Sample runner closing");
+        }
+
+        private void PrintAvaliableCodeSamples()
+        {
+            Console.WriteLine("Select the sample to run by entering it's number. Press Enter to Close.");
+
+            var samples = GetAllSamplesNames();
+            var i = 1;
+
+            foreach(var sample in samples)
+            {
+                Console.WriteLine(i++ + " " + sample);
+            }
+        }
+
+        private string TryParseInputIntoSampleNameToRun(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return string.Empty;
+
+            var parseResult = int.TryParse(input, out int sampleNumber);
+
+            if (!parseResult || !IsSampleNumberValid(sampleNumber)) return string.Empty;
+
+            return GetSampleNameToRun(sampleNumber);
+        }
+
+        private string GetSampleNameToRun(int sampleNumber)
+        {
+            return GetAllSamplesNames()[sampleNumber - 1];
+        }
+
+        private bool IsEndingCondition(string sampleNameToRun)
+        {
+            return string.IsNullOrEmpty(sampleNameToRun);
+        }
+
+        private List<string> GetAllSamplesNames()
         {
             return _codeSamples.Select(s => s.GetName()).ToList();
         }
 
-        public string RunSample(string sampleName)
+        private bool IsSampleNumberValid(int sampleNumber)
+        {
+            return sampleNumber >= 1 || sampleNumber <= GetAllSamplesNames().Count;
+        }
+
+        private string RunSample(string sampleName)
         {
             var sample = _codeSamples.FirstOrDefault(s => string.Equals(s.GetName(), sampleName));
 
